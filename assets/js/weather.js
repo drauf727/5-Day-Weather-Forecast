3cd867faed47cdc510ea1f172fdee47cc7452592
David Rauf <davidrauf@gmail.com>
2024-04-13 00:40:15 -0400

    working5day

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -26,8 +26,8 @@ async function getWeather(city){
     document.getElementById("cityname").innerHTML = data.name;
     document.getElementById("todaydate").innerHTML = `${(day0.getMonth()+1)}/${day0.getDate()}/${day0.getFullYear()}`;
     document.getElementById("todaytemp").innerHTML = `Temperature: ${(Math.round((data.main.temp-273.15)*1.8+32))} °F`;
-    document.getElementById("todayhumidity").innerHTML = `Humidity: ${data.main.humidity}%`;
     document.getElementById("todaywind").innerHTML = `Wind Speed: ${data.wind.speed} MPH`;
+    document.getElementById("todayhumidity").innerHTML = `Humidity: ${data.main.humidity}%`;
 }
 
 // Function for 5 day forecast (pulling from seperate API call)
@@ -35,6 +35,33 @@ async function getWeather5day(city){
     const apiData5 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`);
     var data5 = await apiData5.json();
     console.log(data5);
+    
+    document.getElementById("day1date").innerHTML = `${(day1.getMonth()+1)}/${day1.getDate()}/${day1.getFullYear()}`;
+    document.getElementById("day1temp").innerHTML = `Temperature: ${(Math.round((data5.list[0].main.temp-273.15)*1.8+32))}°F`;
+    document.getElementById("day1wind").innerHTML = `Wind Speed: ${data5.list[0].wind.speed} MPH`;
+    document.getElementById("day1humidity").innerHTML = `Humidity: ${data5.list[0].main.humidity}%`;
+
+    document.getElementById("day2date").innerHTML = `${(day2.getMonth()+1)}/${day2.getDate()}/${day2.getFullYear()}`;
+    document.getElementById("day2temp").innerHTML = `Temperature: ${(Math.round((data5.list[8].main.temp-273.15)*1.8+32))} °F`;
+    document.getElementById("day2wind").innerHTML = `Wind Speed: ${data5.list[8].wind.speed} MPH`;
+    document.getElementById("day2humidity").innerHTML = `Humidity: ${data5.list[8].main.humidity}%`;
+
+    document.getElementById("day3date").innerHTML = `${(day3.getMonth()+1)}/${day3.getDate()}/${day3.getFullYear()}`;
+    document.getElementById("day3temp").innerHTML = `Temperature: ${(Math.round((data5.list[16].main.temp-273.15)*1.8+32))} °F`;
+    document.getElementById("day3wind").innerHTML = `Wind Speed: ${data5.list[16].wind.speed} MPH`;
+    document.getElementById("day3humidity").innerHTML = `Humidity: ${data5.list[16].main.humidity}%`;
+
+
+    document.getElementById("day4date").innerHTML = `${(day4.getMonth()+1)}/${day4.getDate()}/${day4.getFullYear()}`;
+    document.getElementById("day4temp").innerHTML = `Temperature: ${(Math.round((data5.list[24].main.temp-273.15)*1.8+32))} °F`;
+    document.getElementById("day4wind").innerHTML = `Wind Speed: ${data5.list[24].wind.speed} MPH`;
+    document.getElementById("day4humidity").innerHTML = `Humidity: ${data5.list[24].main.humidity}%`;
+
+
+    document.getElementById("day5date").innerHTML = `${(day5.getMonth()+1)}/${day5.getDate()}/${day5.getFullYear()}`;
+    document.getElementById("day5temp").innerHTML = `Temperature: ${(Math.round((data5.list[32].main.temp-273.15)*1.8+32))} °F`;
+    document.getElementById("day5wind").innerHTML = `Wind Speed: ${data5.list[32].wind.speed} MPH`;
+    document.getElementById("day5humidity").innerHTML = `Humidity: ${data5.list[32].main.humidity}%`;
 }
 
 // Event listener to gather city from user
